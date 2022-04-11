39aefe4d20807d52b1b0f24679e3470d9ec03771
Sehal Sein <sehal.sein@fibonalabs.com>
2022-04-11 12:12:21 +0530

    refactor: 💡 Updated Packages
    Updated Packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import 'antd/dist/antd.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+// @ts-ignore
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
